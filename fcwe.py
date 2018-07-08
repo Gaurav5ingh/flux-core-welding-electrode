40b1bfc01a10b52251428385beb48343c4974ea4
Gaurav5ingh <36332787+Gaurav5ingh@users.noreply.github.com>
2018-07-08 18:33:00 +0530

    Update and rename fcwe2.py to fcwe.py

---
+++ fcwe.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import  ttk
+LARGE_FONT = ("Verdana", 12)
+
+
+
+
+
+if __name__ == '__main__':
+    app = pageController()
+    app.mainloop()
